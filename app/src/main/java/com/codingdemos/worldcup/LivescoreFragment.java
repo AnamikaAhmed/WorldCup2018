1e748871959d085302ab96cc5a3a74f3183dc432
AnamikaAhmed <anamika_ahmed@yahoo.com>
2018-06-07 16:34:04 +0600

    Initial commit

---
+++ app/src/main/java/com/codingdemos/worldcup/LivescoreFragment.java
@@ -0,0 +1,29 @@
+package com.codingdemos.worldcup;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LivescoreFragment extends Fragment {
+
+    View view;
+
+    public LivescoreFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_livescore, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/codingdemos/worldcup/ScheduleFragment.java
@@ -0,0 +1,29 @@
+package com.codingdemos.worldcup;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ScheduleFragment extends Fragment {
+
+    View view;
+
+    public ScheduleFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        return view;
+    }
+}
